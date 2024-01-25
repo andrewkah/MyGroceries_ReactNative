151767a1f374f8fff1d8f7256de25e831b38a374
andrewkah <107668588+andrewkah@users.noreply.github.com>
2024-01-25 15:36:37 +0300

    Updated authStack

--- navigation/AuthStack.js
+++ navigation/AuthStack.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import LoginScreen from "../screens/LoginScreen";
-import RegisterScreen from "../screens/RegisterScreen";
+import LoginScreen from "../screens/auth/LoginScreen";
+import RegisterScreen from "../screens/auth/RegisterScreen";
 
 const Stack = createNativeStackNavigator();
 
